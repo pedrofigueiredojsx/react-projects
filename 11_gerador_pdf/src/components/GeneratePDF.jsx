ec4a9421b9621e942543734d5a2806d5ce1c01d6
Pedro Figueiredo <pedrofigueiredojs@gmail.com>
2025-11-13 18:51:40 -0300

    feat: generate pdf done

--- 11_gerador_pdf/src/components/GeneratePDF.jsx
+++ 11_gerador_pdf/src/components/GeneratePDF.jsx
@@ -2,20 +2,55 @@ import React from 'react'
 import TextStyleConfig from './TextStyleConfig'
 import ImageUpload from './ImageUpload'
 
+import { useState } from 'react'
+
+import pdfMake from 'pdfmake/build/pdfmake'
+import pdfFonts from 'pdfmake/build/vfs_fonts'
+
+pdfMake.vfs = pdfFonts.vfs
+
 const GeneratePDF = () => {
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+
+  const generatePdf = () => {
+    // Configurações de estilos
+
+    const documentDefinition = {
+      content: [
+        { text: `Título: ${title}` },
+        { text: `Descrição: ${description}` },
+      ],
+    }
+
+    pdfMake.createPdf(documentDefinition).download()
+  }
+
   return (
     <div className='container'>
       <label className='label'>
         Título:
-        <input type='text' className='input' />
+        <input
+          type='text'
+          className='input'
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
       </label>
       <label className='label'>
         Descrição:
-        <input type='text' className='input' />
+        <input
+          type='text'
+          className='input'
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
       </label>
       <TextStyleConfig />
       <ImageUpload />
-      <button className='button'>Gerar PDF</button>
+      <button className='button' onClick={generatePdf}>
+        Gerar PDF
+      </button>
     </div>
   )
 }
