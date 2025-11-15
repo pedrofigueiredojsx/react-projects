58db732331683b790ab75b3a56d2ec8726f07b4c
Pedro Figueiredo <pedrofigueiredojs@gmail.com>
2025-11-15 17:55:50 -0300

    feat: gerador de pdf done

--- 11_gerador_pdf/src/components/GeneratePDF.jsx
+++ 11_gerador_pdf/src/components/GeneratePDF.jsx
@@ -16,6 +16,7 @@ const GeneratePDF = () => {
   const [fontSize, setFontSize] = useState('12')
   const [fontColor, setFontColor] = useState('#000')
   const [isBold, setIsBold] = useState(false)
+  const [image, setImage] = useState(null)
 
   const generatePdf = () => {
     const customStyle = {
@@ -28,6 +29,7 @@ const GeneratePDF = () => {
       content: [
         { text: `Título: ${title}`, style: 'customStyle' },
         { text: `Descrição: ${description}`, style: 'customStyle' },
+        image ? { image: image, width: 150 } : {},
       ],
       styles: {
         customStyle: customStyle,
@@ -65,7 +67,7 @@ const GeneratePDF = () => {
         isBold={isBold}
         setIsBold={setIsBold}
       />
-      <ImageUpload />
+      <ImageUpload setImage={setImage} />
       <button className='button' onClick={generatePdf}>
         Gerar PDF
       </button>
