b7b0ba6694106aad58de578c6a2930a7a8fdb435
Pedro Figueiredo <pedrofigueiredojs@gmail.com>
2025-11-19 13:42:19 -0300

    feat: pomodoro done

--- 13_pomodoro/src/components/PomodoroTimer.jsx
+++ 13_pomodoro/src/components/PomodoroTimer.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, use } from "react";
 import {
   Box,
   Button,
@@ -11,19 +11,73 @@ import {
 } from "@chakra-ui/react";
 
 const PomodoroTimer = () => {
+  const [minutes, setMinutes] = useState(25);
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+  const [userTime, setUserTime] = useState(25);
+
+  useEffect(() => {
+    let interval;
+
+    if (isActive && (minutes > 0 || seconds > 0)) {
+      interval = setInterval(() => {
+        if (seconds === 0) {
+          setMinutes(minutes - 1);
+          setSeconds(59);
+        } else {
+          setSeconds(seconds - 1);
+        }
+      }, 1000);
+    } else if (minutes === 0 && seconds === 0) {
+      alert("Tempo esgotado!");
+    } else {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [isActive, minutes, seconds]);
+
+  const toggleTimer = () => {
+    setIsActive(!isActive);
+  };
+
+  const resetTimer = () => {
+    setIsActive(false);
+    setMinutes(userTime);
+    setSeconds(0);
+  };
+
+  const handleTimeChange = (e) => {
+    setUserTime(e.target.value);
+    setMinutes(e.target.value);
+  };
+
   return (
     <Center>
       <Box width="100%" maxWidth="400px" p="4">
         <Text fontSize="4xl" mb="4" textAlign="center">
-          10:00
+          {String(minutes).padStart(2, "0")} :{" "}
+          {String(seconds).padStart(2, "0")}
         </Text>
         <FormControl mb="4">
           <FormLabel>Difinir tempo(minutos)</FormLabel>
-          <Input type="number" />
+          <Input
+            type="number"
+            value={userTime}
+            onChange={handleTimeChange}
+            isDisabled={isActive}
+          />
         </FormControl>
         <HStack spacing="4">
-          <Button>Iniciar</Button>
-          <Button colorScheme="gray">Reiniciar</Button>
+          <Button
+            onClick={toggleTimer}
+            colorScheme={isActive ? "red" : "green"}
+          >
+            {isActive ? "Pausar" : "Iniciar"}
+          </Button>
+          <Button colorScheme="gray" onClick={resetTimer}>
+            Reiniciar
+          </Button>
         </HStack>
       </Box>
     </Center>
