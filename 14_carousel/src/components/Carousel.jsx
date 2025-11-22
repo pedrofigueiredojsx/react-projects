8f7194492aea67de9baa898ff21d6f83512e5dfc
Pedro Figueiredo <pedrofigueiredojs@gmail.com>
2025-11-22 16:14:24 -0300

    feat: carousel done

--- 14_carousel/src/components/Carousel.jsx
+++ 14_carousel/src/components/Carousel.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Slide from './Slide'
 import Indicators from './Indicators'
 
@@ -6,14 +6,40 @@ const Carousel = ({ imageUrls }) => {
   const [activeIndex, setActiveIndex] = useState(0)
   const [manualChange, setManualChange] = useState(false)
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (!manualChange) {
+        setActiveIndex((prevIndex) => (prevIndex + 1) % imageUrls.length)
+      }
+      setManualChange(false)
+    }, 5000)
+    return () => clearInterval(interval)
+  }, [manualChange, imageUrls.length])
+
+  const goPrev = () => {
+    setManualChange(true)
+    setActiveIndex(
+      (prevIndex) => (prevIndex - 1 + imageUrls.length) % imageUrls.length
+    )
+  }
+
+  const goNext = () => {
+    setManualChange(true)
+    setActiveIndex((prevIndex) => (prevIndex + 1) % imageUrls.length)
+  }
+
   return (
     <div className="carousel">
       {imageUrls.map((url, index) => (
         <Slide key={index} url={url} isActive={activeIndex === index} />
       ))}
       <Indicators activeIndex={activeIndex} length={imageUrls.length} />
-      <button className="carousel-button prev">Anterior</button>
-      <button className="carousel-button next">Próximo</button>
+      <button className="carousel-button prev" onClick={goPrev}>
+        Anterior
+      </button>
+      <button className="carousel-button next" onClick={goNext}>
+        Próximo
+      </button>
     </div>
   )
 }
