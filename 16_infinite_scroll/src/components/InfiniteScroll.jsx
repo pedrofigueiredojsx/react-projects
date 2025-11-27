5f2591b3b270cd4f561292c522e0750cefb8ec54
Pedro Figueiredo <pedrofigueiredojs@gmail.com>
2025-11-27 14:35:35 -0300

    feat: infinite scroll done

--- 16_infinite_scroll/src/components/InfiniteScroll.jsx
+++ 16_infinite_scroll/src/components/InfiniteScroll.jsx
@@ -33,12 +33,32 @@ const InfiniteScroll = () => {
     fetchPosts()
   }, [page])
 
+  const handleScroll = () => {
+    if (
+      window.innerHeight + window.scrollY >=
+        document.documentElement.scrollHeight - 100 &&
+      !isLoading &&
+      hasMoreData
+    ) {
+      setPage((prevPage) => prevPage + 1)
+    }
+  }
+
+  useEffect(() => {
+    const throttleHandleScroll = throttle(handleScroll, 150)
+    window.addEventListener('scroll', throttleHandleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', throttleHandleScroll)
+    }
+  }, [hasMoreData, isLoading])
+
   return (
     <div>
       <h1>Infinite Scroll</h1>
       <ul>
         {posts.map((post) => (
-          <li key={post.id}>
+          <li key={Math.random() * 1000}>
             <h3>{post.title}</h3>
             <p>{post.body}</p>
           </li>
@@ -47,6 +67,17 @@ const InfiniteScroll = () => {
       {isLoading && <p>Carregando mais posts...</p>}
     </div>
   )
+}
+
+function throttle(func, delay) {
+  let lastCall = 0
+
+  return function (...args) {
+    const now = new Date().getTime()
+    if (now - lastCall < delay) return
+    lastCall = now
+    return func(...args)
+  }
 }
 
 export default InfiniteScroll
