6b6cd70eca17a26b97cf005e41e8d06e4c7b520d
Pedro Figueiredo <pedrofigueiredojs@gmail.com>
2025-12-08 23:46:47 -0300

    feat: axios get on jsonplaceholder api

--- 18_blog_axios/src/routes/Home.jsx
+++ 18_blog_axios/src/routes/Home.jsx
@@ -1,6 +1,31 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+
+import { Link } from 'react-router-dom'
+
+import './Home.css'
+import axios from 'axios'
 
 const Home = () => {
+  const [posts, setPost] = useState([])
+
+  const getPosts = async () => {
+    try {
+      const response = await axios.get(
+        'https://jsonplaceholder.typicode.com/posts'
+      )
+
+      const data = response.data
+
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getPosts()
+  }, [])
+
   return <div>Home</div>
 }
 
