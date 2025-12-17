70211559993e91a871aff9e96571b3e94f2b0cae
Pedro Figueiredo <pedrofigueiredojs@gmail.com>
2025-12-17 12:49:54 -0300

    feat: blog edit post with axios put and done project

--- 18_blog_axios/src/routes/EditPost.jsx
+++ 18_blog_axios/src/routes/EditPost.jsx
@@ -10,6 +10,31 @@ const EditPost = () => {
 
   const { id } = useParams()
 
+  const getPost = async () => {
+    try {
+      const response = await blogFetch.get(`/posts/${id}`)
+
+      const data = response.data
+
+      setTitle(data.title)
+      setBody(data.body)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const editPost = async (e) => {
+    e.preventDefault()
+
+    const post = { title, body, userId: 1 }
+
+    await blogFetch.put(`/posts/${id}`, { body: post })
+  }
+
+  useEffect(() => {
+    getPost()
+  }, [])
+
   return (
     <div className="new-post">
       <h2>Editando: {title}</h2>
@@ -22,6 +47,7 @@ const EditPost = () => {
             id="title"
             placeholder="Digite o título"
             onChange={(e) => setTitle(e.target.value)}
+            value={title || ''}
           />
         </div>
         <div className="form-control">
@@ -30,7 +56,8 @@ const EditPost = () => {
             name="body"
             id="body"
             placeholder="Digite o conteúdo"
-            onChange={(e) => setBody(e.target.value)}></textarea>
+            onChange={(e) => setBody(e.target.value)}
+            value={body || ''}></textarea>
         </div>
         <input type="submit" value="Criar Post" className="btn" />
       </form>
