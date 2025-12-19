a5186021c6b71669629d8b9847dff5a283852ca8
Pedro Figueiredo <pedrofigueiredojs@gmail.com>
2025-12-19 18:07:30 -0300

    feat: form e icons do react icons

--- 19_multistep_form_react/src/App.jsx
+++ 19_multistep_form_react/src/App.jsx
@@ -1,10 +1,36 @@
+// components
+
+import { GrFormNext, GrFormPrevious } from 'react-icons/gr'
+
 import './App.css'
 
 function App() {
   return (
     <>
-      <div>
-        <h1>React Multistep</h1>
+      <div className="app">
+        <div className="header">
+          <h2>Deixe sua avaliação</h2>
+          <p>
+            Ficamos felizes com a sua compra, utilize o formulário abaixo para
+            avaliar o produto
+          </p>
+        </div>
+        <div className="form-container">
+          <p>etapas</p>
+          <form>
+            <div className="inputs-container"></div>
+            <div className="actions">
+              <button type="button">
+                <GrFormPrevious />
+                <span>Voltar</span>
+              </button>
+              <button type="submit">
+                <span>Avançar</span>
+                <GrFormNext />
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </>
   )
