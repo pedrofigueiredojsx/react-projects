9fa9e57bbb0f1e2a02d6a43d9b708c5c13fd1368
Pedro Figueiredo <pedrofigueiredojs@gmail.com>
2025-12-22 20:14:31 -0300

    feat: create inputs e css

--- 19_multistep_form_react/src/components/UserForm.jsx
+++ 19_multistep_form_react/src/components/UserForm.jsx
@@ -3,7 +3,26 @@ import React from 'react'
 const UserForm = () => {
   return (
     <div>
-      <h2>User Form</h2>
+      <div className="form-control">
+        <label htmlFor="name">Nome:</label>
+        <input
+          type="text"
+          name="text"
+          id="name"
+          placeholder="Digite o seu nome"
+          required
+        />
+      </div>
+      <div className="form-control">
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Digite o seu email"
+          required
+        />
+      </div>
     </div>
   )
 }
