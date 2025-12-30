2e96b7a813e9b31b81c5cc2047dd2cfbc5edbf0d
Pedro Figueiredo <pedrofigueiredojs@gmail.com>
2025-12-30 15:54:31 -0300

    feat: props

--- 20_github_api/src/routes/Home.tsx
+++ 20_github_api/src/routes/Home.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import { UserProps } from '../types/user'
+
+import Search from '../components/Search'
+
+import { useState } from 'react'
 
 const Home = () => {
-  return <div>Home</div>
+  const [user, setUser] = useState<UserProps | null>(null)
+
+  const loadUser = async (userName: string) => {
+    const res = await fetch(`https://api.github.com/users`)
+    const data = await res.json()
+
+    setUser(data)
+    console.log(data)
+  }
+
+  return (
+    <div>
+      <Search loadUser={loadUser} />
+    </div>
+  )
 }
 
 export default Home
